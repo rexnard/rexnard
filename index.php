6e7dd04acd0016992b4012370b3df352209aa487
rexnard <100132176+rexnard@users.noreply.github.com>
2022-02-22 12:32:35 +0800

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <h1>Welcome to My Website! I am Rheyxs Fernandez</h1>
     <ul>
       <li><a href="index.php">HOME</a></li>
-      <li><a href="LT2/recipe.html">Learning Task 2</a></li>
+      <li><a href="Learning Task 2/recipe.html">Learning Task 2</a></li>
     </ul>
   </body>
 </html>
