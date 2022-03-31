5e49518ddb55f8fa997fe35a8244d73fa4ab0cec
rexnard <100132176+rexnard@users.noreply.github.com>
2022-03-31 16:21:13 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+          <title> My Home Page </title>
+    </head>
+    <body>
+    <h1> Welcome to my Website! I'm Rheyxs Fernandez </h1>
+    <ul>
+      <li><a href="index.php">HOME</a></li>
+      <li><a href="Learning Task 2/recipe.html">Learning Task 2</a></li>
+    </ul>
+    </body>
+</html>
